ac38343ab2480130b50cd9b120a20df4ef5835fd
jehanshah8 <jehan.shah8@gmail.com>
2022-11-15 18:07:03 -0500

    added arducam stuff to dockerfile. pending tests

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -2,5 +2,8 @@
 
 CONTAINER_IMAGE=metaporter-nano-ros:1.0
 
-#from the root of the workspace
-docker build -t $CONTAINER_IMAGE -f src/metaporter_nano/docker/Dockerfile .
+#from the root of the workspace stop at builder stage for dev
+docker build --target builder -t $CONTAINER_IMAGE -f src/metaporter_nano/docker/Dockerfile .
+
+#from the root of the workspace, make entire for deploy
+#docker build -t $CONTAINER_IMAGE -f src/metaporter_nano/docker/Dockerfile .
