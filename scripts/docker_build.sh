4b942e368a265f8b7bb9adafa7f87a4e61155ae4
Jehan Shah <jehan.shah8@gmail.com>
2022-11-17 14:11:35 -0500

    format

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,7 +3,7 @@
 CONTAINER_IMAGE=metaporter-nano-ros:1.0
 
 #from the root of the workspace stop at builder stage for dev
-docker build --target build2 -t $CONTAINER_IMAGE -f src/metaporter_nano/docker/Dockerfile .
+docker build --target build1 -t $CONTAINER_IMAGE -f src/metaporter_nano/docker/Dockerfile .
 
 #from the root of the workspace, make entire for deploy
 #docker build -t $CONTAINER_IMAGE -f src/metaporter_nano/docker/Dockerfile .
